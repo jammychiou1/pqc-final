16feded379991df2ba9e3a9a596af7a4ced7591f
jammychiou1 <jammy.chiou1@gmail.com>
2023-07-17 11:25:47 +0800

    karatsuba in basemul

--- src/neon/base_mul.cpp
+++ src/neon/base_mul.cpp
@@ -8,6 +8,9 @@
 #include "arith_tmpl/gen_const.h"
 #include "arith_tmpl/neon_arith.h"
 
+#include <iostream>
+#include "utils/debug.h"
+
 constexpr int ORD = 4590;
 constexpr int16_t W_4590 = 11;
 
@@ -38,78 +41,114 @@ constexpr std::array<std::array<int16_t, 9>, 10> base_mul_twiddle_bars = [] {
 } ();
 
 template<int LANE>
-void base_mul_col(int32x4_t &res_front_low, int32x4_t &res_front_high, int32x4_t &res_back_low, int32x4_t &res_back_high, int16x8_t col_front, int16x8_t col_back, int16x8_t weights) {
-  res_front_low = vmlal_laneq_s16(res_front_low, vget_low_s16(col_front), weights, LANE);
-  res_front_high = vmlal_high_laneq_s16(res_front_high, col_front, weights, LANE);
-  res_back_low = vmlal_laneq_s16(res_back_low, vget_low_s16(col_back), weights, LANE);
-  res_back_high = vmlal_high_laneq_s16(res_back_high, col_back, weights, LANE);
+void base_mul_col_8(int32x4_t &res_low, int32x4_t &res_high, int16x8_t col, int16x8_t weights) {
+  res_low = vmlal_laneq_s16(res_low, vget_low_s16(col), weights, LANE);
+  res_high = vmlal_high_laneq_s16(res_high, col, weights, LANE);
+}
+
+inline void base_mul_8(int32x4_t &res_low, int32x4_t &res_high, int16x8_t m1, int16x8_t m2, int16x8_t v) {
+  base_mul_col_8<0>(res_low, res_high, m2, v);
+  base_mul_col_8<1>(res_low, res_high, vextq_s16(m1, m2, 7), v);
+  base_mul_col_8<2>(res_low, res_high, vextq_s16(m1, m2, 6), v);
+  base_mul_col_8<3>(res_low, res_high, vextq_s16(m1, m2, 5), v);
+  base_mul_col_8<4>(res_low, res_high, vextq_s16(m1, m2, 4), v);
+  base_mul_col_8<5>(res_low, res_high, vextq_s16(m1, m2, 3), v);
+  base_mul_col_8<6>(res_low, res_high, vextq_s16(m1, m2, 2), v);
+  base_mul_col_8<7>(res_low, res_high, vextq_s16(m1, m2, 1), v);
 }
 
 void base_mul(int16_t in1_ntt[10][9][16], int16_t in2_ntt[10][9][16], int16_t out_ntt[10][9][16]) {
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 9; j++) {
-      int32x4_t res_front_low = {};
-      int32x4_t res_front_high = {};
-      int32x4_t res_back_low = {};
-      int32x4_t res_back_high = {};
       int16_t twiddle = base_mul_twiddles[i][j];
       int16_t twiddle_bar = base_mul_twiddle_bars[i][j];
-      int16x8_t in1_front = vld1q_s16(&in1_ntt[i][j][0]);
-      int16x8_t in1_back = vld1q_s16(&in1_ntt[i][j][8]);
-      int16x8_t in2_front = vld1q_s16(&in2_ntt[i][j][0]);
-      int16x8_t in2_back = vld1q_s16(&in2_ntt[i][j][8]);
-      int16x8_t twiddle_in2_front = barret_mul<Q>(in2_front, twiddle, twiddle_bar);
-      int16x8_t twiddle_in2_back = barret_mul<Q>(in2_back, twiddle, twiddle_bar);
+      // std::cerr << "twiddle: " << twiddle << ", " << twiddle_bar << '\n';
 
-      base_mul_col<0>(res_front_low, res_front_high, res_back_low, res_back_high,
-          in2_front, in2_back, in1_front);
-      base_mul_col<1>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_back, in2_front, 7), vextq_s16(in2_front, in2_back, 7), in1_front);
-      base_mul_col<2>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_back, in2_front, 6), vextq_s16(in2_front, in2_back, 6), in1_front);
-      base_mul_col<3>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_back, in2_front, 5), vextq_s16(in2_front, in2_back, 5), in1_front);
-      base_mul_col<4>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_back, in2_front, 4), vextq_s16(in2_front, in2_back, 4), in1_front);
-      base_mul_col<5>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_back, in2_front, 3), vextq_s16(in2_front, in2_back, 3), in1_front);
-      base_mul_col<6>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_back, in2_front, 2), vextq_s16(in2_front, in2_back, 2), in1_front);
-      base_mul_col<7>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_back, in2_front, 1), vextq_s16(in2_front, in2_back, 1), in1_front);
+      int16x8_t a_fr = vld1q_s16(&in1_ntt[i][j][0]);
+      int16x8_t a_bk = vld1q_s16(&in1_ntt[i][j][8]);
 
-      base_mul_col<0>(res_front_low, res_front_high, res_back_low, res_back_high,
-          twiddle_in2_back, in2_front, in1_back);
-      base_mul_col<1>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_front, twiddle_in2_back, 7), vextq_s16(twiddle_in2_back, in2_front, 7), in1_back);
-      base_mul_col<2>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_front, twiddle_in2_back, 6), vextq_s16(twiddle_in2_back, in2_front, 6), in1_back);
-      base_mul_col<3>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_front, twiddle_in2_back, 5), vextq_s16(twiddle_in2_back, in2_front, 5), in1_back);
-      base_mul_col<4>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_front, twiddle_in2_back, 4), vextq_s16(twiddle_in2_back, in2_front, 4), in1_back);
-      base_mul_col<5>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_front, twiddle_in2_back, 3), vextq_s16(twiddle_in2_back, in2_front, 3), in1_back);
-      base_mul_col<6>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_front, twiddle_in2_back, 2), vextq_s16(twiddle_in2_back, in2_front, 2), in1_back);
-      base_mul_col<7>(res_front_low, res_front_high, res_back_low, res_back_high,
-          vextq_s16(twiddle_in2_front, twiddle_in2_back, 1), vextq_s16(twiddle_in2_back, in2_front, 1), in1_back);
+      // std::cerr << "a:\n";
+      // debug_int16x8(a_fr);
+      // debug_int16x8(a_bk);
+
+      int16x8_t a_fb = a_fr;
+      barret_reduce<Q>(a_fb);
+      a_fb = vaddq_s16(a_fb, a_bk);
+
+      // std::cerr << "a_fb:\n";
+      // debug_int16x8(a_fb);
+
+      int16x8_t b_fr = vld1q_s16(&in2_ntt[i][j][0]);
+      int16x8_t b_bk = vld1q_s16(&in2_ntt[i][j][8]);
+
+      // std::cerr << "b:\n";
+      // debug_int16x8(b_fr);
+      // debug_int16x8(b_bk);
+
+      barret_reduce<Q>(b_fr);
+
+      int16x8_t c12 = b_fr;
+      int16x8_t c11 = barret_mul<Q>(b_bk, twiddle, twiddle_bar);
+      int16x8_t c22 = vsubq_s16(b_bk, b_fr);
+      int16x8_t c21 = vsubq_s16(b_fr, c11);
+      int16x8_t c02 = vnegq_s16(c21);
+      int16x8_t c01 = barret_mul<Q>(c22, -twiddle, -twiddle_bar);
+
+      // std::cerr << "c:\n";
+      // debug_int16x8(c12);
+      // debug_int16x8(c11);
+      // debug_int16x8(c22);
+      // debug_int16x8(c21);
+      // debug_int16x8(c02);
+      // debug_int16x8(c01);
+
+      int32x4_t res_fr_low = {};
+      int32x4_t res_fr_high = {};
+
+      base_mul_8(res_fr_low, res_fr_high, c11, c12, a_fb);
+
+      int32x4_t res_bk_low = res_fr_low;
+      int32x4_t res_bk_high = res_fr_high;
+
+      base_mul_8(res_fr_low, res_fr_high, c01, c02, a_bk);
+      base_mul_8(res_bk_low, res_bk_high, c21, c22, a_fr);
 
       int32x4_t esti;
-      esti = vqrdmulhq_n_s32(res_front_low, 467759);
-      res_front_low = vmlsq_n_s32(res_front_low, esti, Q);
-      esti = vqrdmulhq_n_s32(res_front_high, 467759);
-      res_front_high = vmlsq_n_s32(res_front_high, esti, Q);
-      esti = vqrdmulhq_n_s32(res_back_low, 467759);
-      res_back_low = vmlsq_n_s32(res_back_low, esti, Q);
-      esti = vqrdmulhq_n_s32(res_back_high, 467759);
-      res_back_high = vmlsq_n_s32(res_back_high, esti, Q);
+      esti = vqrdmulhq_n_s32(res_fr_low, 467759);
+      res_fr_low = vmlsq_n_s32(res_fr_low, esti, Q);
+      esti = vqrdmulhq_n_s32(res_fr_high, 467759);
+      res_fr_high = vmlsq_n_s32(res_fr_high, esti, Q);
+      esti = vqrdmulhq_n_s32(res_bk_low, 467759);
+      res_bk_low = vmlsq_n_s32(res_bk_low, esti, Q);
+      esti = vqrdmulhq_n_s32(res_bk_high, 467759);
+      res_bk_high = vmlsq_n_s32(res_bk_high, esti, Q);
 
-      int16x8_t res_front = vuzp1q_s16(vreinterpretq_s16_s32(res_front_low), vreinterpretq_s16_s32(res_front_high));
-      int16x8_t res_back = vuzp1q_s16(vreinterpretq_s16_s32(res_back_low), vreinterpretq_s16_s32(res_back_high));
+      int16x8_t res_fr = vuzp1q_s16(vreinterpretq_s16_s32(res_fr_low), vreinterpretq_s16_s32(res_fr_high));
+      int16x8_t res_bk = vuzp1q_s16(vreinterpretq_s16_s32(res_bk_low), vreinterpretq_s16_s32(res_bk_high));
 
-      vst1q_s16(&out_ntt[i][j][0], res_front);
-      vst1q_s16(&out_ntt[i][j][8], res_back);
+      // std::cerr << "res:\n";
+      // debug_int16x8(res_fr);
+      // debug_int16x8(res_bk);
+
+      // int64_t tmp[16] = {};
+      // for (int k1 = 0; k1 < 16; k1++) {
+      //   for (int k2 = 0; k2 < 16; k2++) {
+      //     if (k1 + k2 >= 16) {
+      //       tmp[k1 + k2 - 16] += int64_t(twiddle) * in1_ntt[i][j][k1] * in2_ntt[i][j][k2];
+      //     }
+      //     else {
+      //       tmp[k1 + k2] += int64_t(1) * in1_ntt[i][j][k1] * in2_ntt[i][j][k2];
+      //     }
+      //   }
+      // }
+      // std::cerr << "reference:\n";
+      // for (int k = 0; k < 16; k++) {
+      //   std::cerr << center_lift<int64_t, Q>(tmp[k]) << " \n"[k == 15];
+
+      // }
+
+      vst1q_s16(&out_ntt[i][j][0], res_fr);
+      vst1q_s16(&out_ntt[i][j][8], res_bk);
     }
   }
 }
